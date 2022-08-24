b1cff6ee6d93f7ddd6fdd46269863e431a51e1b7
lecture-yonggishin <lecture.yonggishin@gmail.com>
2022-08-25 02:17:16 +0900

    update publish test6

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React : edit
+          Learn React : edit2222
         </a>
       </header>
     </div>
